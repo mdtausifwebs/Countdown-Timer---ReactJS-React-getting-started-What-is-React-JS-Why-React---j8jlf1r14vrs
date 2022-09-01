e8f2fa9059374a0ac18806eafa6450c0d8f5cb97
tausif <tausifsmo.com>
2022-09-01 22:38:47 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,25 @@ import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  // write your code here 
-
+  const [inputTimer, setinputTimer] = useState(null);
+  console.log('inputTimer', inputTimer);
+  const [intern, Setintern] = useState(null);
+  const onkeydownss = () => {
+    if (inputTimer != null) {
+     let ineterns =  setInterval(()=>{
+        setinputTimer((pre) => pre - 1)
+      }, 1000)
+      Setintern(ineterns)
+    }
+  }
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={inputTimer ? onkeydownss : clearInterval(intern)} onChange={(e) => setinputTimer(e.target.value)} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{inputTimer && inputTimer}</div>
     </div>
   )
 }
