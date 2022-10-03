69582cd9778e4cfd2e9b0bbb386a9e8a55522627
tausif <tausifsmo.com>
2022-10-04 00:10:03 +0530

    some change

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,35 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
   const [inputTimer, setinputTimer] = useState(null);
   const [intern, Setintern] = useState(null);
   const onkeydownss = () => {
-    Setintern(null)
+    Setintern(null);
     if (inputTimer != null) {
-     let ineterns =  setInterval(()=>{
-        setinputTimer((pre) => pre - 1)
-      }, 1000)
-      Setintern(ineterns)
+      let ineterns = setInterval(() => {
+        setinputTimer((pre) => pre - 1);
+        console.log(inputTimer);
+      }, 1000);
+      Setintern(ineterns);
     }
-  }
+  };
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyPress={inputTimer ? onkeydownss : clearInterval(intern)} onChange={(e) => setinputTimer(e.target.value)} /> sec.
+          Reverse countdown for
+          <input
+            id="timeCount"
+            onKeyPress={inputTimer ? onkeydownss : clearInterval(intern)}
+            onChange={(e) => setinputTimer(e.target.value)}
+          />{" "}
+          sec.
         </h1>
       </div>
-      <div id="current-time">{inputTimer}</div>
+      <div id="current-time">{inputTimer && inputTimer}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
